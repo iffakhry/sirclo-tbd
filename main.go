821683b5a8562c3002442f3e9f4f2f6580858b31
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-01-14 15:16:28 +0700

    clean rebase comment on main.go

--- main.go
+++ main.go
@@ -8,14 +8,8 @@ func main() {
 	fmt.Println("hello commit 1")
 	fmt.Println("hello commit 2")
 	fmt.Println("hello commit 3")
-<<<<<<< HEAD
-<<<<<<< HEAD
 	fmt.Println("hello commit 4")
 	fmt.Println("hello commit 5")
-=======
 	fmt.Println("hello from feature-rebase")
->>>>>>> 0833d83 (add print from feature-rebase)
-=======
 	fmt.Println("hello from feature-rebase")
->>>>>>> 0833d838a38f4e94c624b8447e449acda1fd213b
 }
