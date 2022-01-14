be99c76d6c86f65a1168b176856ecaa2ab2a2f6f
Fakhry-alta <fakhry.ikhsan.firdaus@gmail.com>
2022-01-14 15:05:24 +0700

    add print from feature-rebase

--- main.go
+++ main.go
@@ -8,6 +8,10 @@ func main() {
 	fmt.Println("hello commit 1")
 	fmt.Println("hello commit 2")
 	fmt.Println("hello commit 3")
+<<<<<<< HEAD
 	fmt.Println("hello commit 4")
 	fmt.Println("hello commit 5")
+=======
+	fmt.Println("hello from feature-rebase")
+>>>>>>> 0833d83 (add print from feature-rebase)
 }
